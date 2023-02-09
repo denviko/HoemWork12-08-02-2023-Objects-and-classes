2cddfa70ee9b376eae5150d0c7a22c70aefd0238
DenViko <DenViko@yahoo.com>
2023-02-09 05:42:47 +0500

    HW12

--- src/HW12/MainHW12ObjectsAndClasses.java
+++ src/HW12/MainHW12ObjectsAndClasses.java
@@ -1,4 +1,30 @@
 package HW12;
 
+
 public class MainHW12ObjectsAndClasses {
+
+    public static void main(String[] args) {
+        Book warAndPeace = new Book ("War and Peace",1869);
+        System.out.println("warAndPeace.bookName = " + warAndPeace.getBookName());
+        System.out.println("warAndPeace.publicationYear = " + warAndPeace.getPublicationYear());
+        warAndPeace.setPublicationYear(1875);
+        System.out.println("warAndPeace.getPublicationYear() = " + warAndPeace.getPublicationYear());
+
+        Book theMasterAndMargarita = new Book("The Master and Margarita",1967);
+        System.out.println("theMasterAndMargarita.bookName = " + theMasterAndMargarita.getBookName());
+        System.out.println("theMasterAndMargarita.publicationYear = " + theMasterAndMargarita.getPublicationYear());
+        theMasterAndMargarita.setPublicationYear(1988);
+        System.out.println("theMasterAndMargarita.getPublicationYear() = " + theMasterAndMargarita.getPublicationYear());
+
+        Author leo = new Author("Leo Tolstoy");
+        System.out.println("leo.authorName = " + leo.getAuthorName());
+
+        Author mikhail = new Author("Mihkail Bulgakov");
+        System.out.println("mikhail.authorName = " + mikhail.getAuthorName());
+
+
+    }
+
+
+
 }
